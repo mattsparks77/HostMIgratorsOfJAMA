e38934d5538bff074e910515fc8480cb2060e2d0
Avi Miller <TrebecS@Gmail.com>
2018-06-13 16:30:17 -0700

    Added camera controls

---
+++ Assets/CameraControls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 3f;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 camPosition = Vector3.zero;
+        camPosition += Vector3.right * ((Input.GetKey(KeyCode.D)?1:0) + (Input.GetKey(KeyCode.A)?-1:0));
+        camPosition += Vector3.up * ((Input.GetKey(KeyCode.W) ? 1 : 0) + (Input.GetKey(KeyCode.S) ? -1 : 0));
+        camPosition *= Time.deltaTime * moveSpeed;
+        transform.position += camPosition;
+	}
+}
