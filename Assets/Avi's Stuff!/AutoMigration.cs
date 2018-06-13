a608402a7b90899b0896570a3b64cc52611a6729
Avi Miller <TrebecS@Gmail.com>
2018-06-13 16:03:35 -0700

    Added automatic host migration

--- Assets/Avi's Stuff!/AutoMigration.cs
+++ Assets/Avi's Stuff!/AutoMigration.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
 
-public class AutoMigration : NetworkBehaviour {
+public class AutoMigration : NetworkMigrationManager {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] float reconnectDelay = 1f;
+    [SerializeField] int maxReconnectAttempts = 10;
+    IEnumerator reconnectCoroutine;
+
+    protected override void OnClientDisconnectedFromHost(NetworkConnection conn, out SceneChangeOption sceneChange) {
+        //base.OnClientDisconnectedFromHost(conn, out sceneChange);
+        sceneChange = SceneChangeOption.StayInOnlineScene;
+
+        PeerInfoMessage infoMessage;
+        bool isNewHost;
+        if(FindNewHost(out infoMessage, out isNewHost)){
+            if(isNewHost){
+                BecomeNewHost(infoMessage.port);
+            } else {
+                reconnectCoroutine = AttemptReconnect(reconnectDelay, infoMessage.address, infoMessage.port);
+                StartCoroutine(reconnectCoroutine);
+            }
+        }
+
+    }
+
+    IEnumerator AttemptReconnect(float delay, string address, int port){
+        for (int i = 0; i < maxReconnectAttempts; ++i){
+            yield return new WaitForSecondsRealtime(delay);
+            client.ReconnectToNewHost(address, port);
+        }
+    }
+
 }
---
+++ Assets/Avi's Stuff!/AutoNetwork.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AutoNetwork : NetworkManager {
+
+    /// <summary>
+    /// If this client is the host:
+    /// This class should send a message to the Unity matchmaking service
+    /// to start a match, and provide its public IP address so that other
+    /// users can connect directly.
+    /// 
+    /// Does this include a port number? Or would it be better for all hosts
+    /// to use a static port? Or is that impossible with the NAT in the way?
+    /// 
+    /// If this client is not the host:
+    /// Look for any match on the Unity matchmaking service and connect
+    /// directly to the address found there.
+    /// </summary>
+
+    void LookForActiveHost(){
+        
+    }
+
+    void BecomeActiveHost(){
+        
+    }
+
+}
