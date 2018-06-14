4af3fbd8d819cf7338318d53f8e7e96d7c548779
Andy Le <azuredragon013@yahoo.com>
2018-06-13 17:47:20 -0700

    TitleScreen scenes
    Setup TitleScreen, play button currently loads Matt's Test Scene

---
+++ Assets/TouchControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControls : MonoBehaviour {
+    public float perspectiveSpeed = 0.5f;
+    public float orthoZoomSpeed = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+            if(GetComponent<Camera>().orthographic)
+            {
+                GetComponent<Camera>().orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+                GetComponent<Camera>().orthographicSize = Mathf.Max(GetComponent<Camera>().orthographicSize, 0.1f);
+
+            }
+            else
+            {
+                GetComponent<Camera>().fieldOfView += deltaMagnitudeDiff * perspectiveSpeed;
+                GetComponent<Camera>().fieldOfView = Mathf.Clamp(GetComponent<Camera>().fieldOfView, 0.1f, 179.9f);
+            }
+        }
+
+
+		
+	}
+}
